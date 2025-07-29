04f0947d5c116f72ef21f89e386c906d058ecfb9
John Obaje <jobaje705@gmail.com>
2025-07-29 02:49:25 +0100

    added build script

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -1,6 +1,6 @@
 import os
 from pathlib import Path
-
+from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -9,7 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(*h%h-rc^kh_ob18is3j+c6@*3b92&9p^%s2lf^qas^lk3*n_q'
+SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = 'LibraryManagementSystem.urls'
@@ -72,6 +74,13 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join('/code/db', 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/venv bash
+
+# Exit on error
+set -o errexit
+
+
+# Install dependencies
+pip install -r requirements.txt
+
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations
+python manage.py migrate
+
