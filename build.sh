046432ade10455cbca8c4cfcc81163882c701488
John Obaje <jobaje705@gmail.com>
2025-07-30 17:37:14 +0100

    UPDATE

--- build.sh
+++ build.sh
@@ -1,15 +1,27 @@
-#!/usr/bin/venv bash
-
-# Exit on error
+#!/usr/bin/env bash
+# exit on error
 set -o errexit
-
 
 # Install dependencies
 pip install -r requirements.txt
 
+# Run migrations to create database tables
+python manage.py migrate
 
 # Collect static files
-python manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
-
-
+# Create superuser automatically (only if it doesn't exist)
+python manage.py shell -c "
+import os
+from django.contrib.auth import get_user_model
+User = get_user_model()
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', 'hello123@@')
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print('Superuser created successfully')
+else:
+    print('Superuser already exists')
+"
